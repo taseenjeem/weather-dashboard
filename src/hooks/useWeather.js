a3554629926d301b9e9838ecc73428378da2b9b1
taseenjeem <taseenjeem.dev@gmail.com>
2024-02-12 14:06:52 +0600

    Added custom hook

---
+++ src/hooks/useWeather.js
@@ -0,0 +1,89 @@
+import { useEffect, useState } from "react";
+
+const useWeather = () => {
+  const [weatherData, setWeatherData] = useState({
+    location: "",
+    climate: "",
+    temperature: "",
+    minTemperature: "",
+    maxTemperature: "",
+    humidity: "",
+    cloudPercentage: "",
+    wind: "",
+    time: "",
+    longitude: "",
+    latitude: "",
+  });
+
+  const [loading, setLoading] = useState({
+    status: false,
+    message: "",
+  });
+
+  const [error, setError] = useState(null);
+
+  const fetchWeatherData = async (latitude, longitude) => {
+    try {
+      setLoading({
+        ...loading,
+        status: true,
+        message: "Fetching weather data. Please Wait!",
+      });
+
+      const response = await fetch(
+        `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${
+          import.meta.env.VITE_WEATHER_API_KEY
+        }&units=metric`
+      );
+
+      if (!response.ok) {
+        const errorMessage = `Fetching data failed: Error status - ${response.status}`;
+
+        throw new Error(errorMessage);
+      }
+
+      const data = await response.json();
+
+      const updatedWeatherData = {
+        ...weatherData,
+        location: data?.name,
+        climate: data?.weather[0]?.main,
+        temperature: data?.main?.temp,
+        minTemperature: data?.main?.temp_max,
+        maxTemperature: data?.main?.temp_min,
+        humidity: data?.main?.humidity,
+        cloudPercentage: data?.clouds?.all,
+        wind: data?.wind?.speed,
+        time: data?.dt,
+        longitude: longitude,
+        latitude: latitude,
+      };
+
+      setWeatherData(updatedWeatherData);
+    } catch (err) {
+      console.log(err);
+      setError(err);
+    } finally {
+      setLoading({
+        ...loading,
+        status: false,
+        message: "",
+      });
+    }
+  };
+
+  useEffect(() => {
+    setLoading({
+      status: true,
+      message: "Finding your location..",
+    });
+
+    navigator.geolocation.getCurrentPosition((position) => {
+      fetchWeatherData(position.coords.latitude, position.coords.longitude);
+    });
+  }, []);
+
+  return { weatherData, error, loading };
+};
+
+export default useWeather;
