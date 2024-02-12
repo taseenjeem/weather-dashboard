27c33ebc941aa28f3b41c7d432694b542a7cd198
taseenjeem <taseenjeem.dev@gmail.com>
2024-02-12 16:25:20 +0600

    dynamic the weather headline

--- src/components/weather/WeatherBoard.jsx
+++ src/components/weather/WeatherBoard.jsx
@@ -1,21 +1,28 @@
-import { useWeather } from "../../hooks";
+import { useContext } from "react";
 import AddToFavorite from "./AddToFavorite";
 import WeatherCondition from "./WeatherCondition";
 import WeatherHeadline from "./WeatherHeadline";
+import { WeatherContext } from "../../context";
 
 const WeatherBoard = () => {
-  const { loading, error, weatherData } = useWeather();
-
-  console.log(loading, error, weatherData);
+  const { loading } = useContext(WeatherContext);
 
   return (
     <>
       <div className="container">
         <div className="grid bg-black/20 rounded-xl backdrop-blur-md border-2 lg:border-[3px] border-white/[14%] px-4 lg:px-14 py-6 lg:py-10 min-h-[520px] max-w-[1058px] mx-auto">
           <div className="grid md:grid-cols-2 gap-10 md:gap-6">
-            <AddToFavorite />
-            <WeatherHeadline />
-            <WeatherCondition />
+            {loading.status ? (
+              <div className="min-h-[70vh] flex justify-center items-center">
+                <p>{loading.message}</p>
+              </div>
+            ) : (
+              <>
+                <AddToFavorite />
+                <WeatherHeadline />
+                <WeatherCondition />
+              </>
+            )}
           </div>
         </div>
       </div>
--- src/components/weather/WeatherHeadline.jsx
+++ src/components/weather/WeatherHeadline.jsx
@@ -1,7 +1,14 @@
+import { useContext } from "react";
 import cloudIcon from "../../assets/cloud.svg";
 import pinIcon from "../../assets/pin.svg";
+import { WeatherContext } from "../../context";
+import formattedDate from "../../utils/date-utils";
 
 const WeatherHeadline = () => {
+  const { weatherData } = useContext(WeatherContext);
+
+  console.log(weatherData);
+
   return (
     <>
       <div>
@@ -9,15 +16,20 @@ const WeatherHeadline = () => {
           <img src={cloudIcon} alt="cloud" />
           <div className="max-md:flex items-center max-md:space-x-4">
             <h1 className="text-[60px] lg:text-[80px] xl:text-[100px] leading-none md:mb-4">
-              16°
+              {weatherData?.temperature}°
             </h1>
             <div className="flex items-center space-x-4 md:mb-4">
               <img src={pinIcon} />
-              <h2 className="text-2xl lg:text-[50px]">Dhaka</h2>
+              <h2 className="text-2xl lg:text-[50px]">
+                {weatherData?.location}
+              </h2>
             </div>
           </div>
         </div>
-        <p className="text-sm lg:text-lg">06:09 - Sunday, 9 Dec ‘23</p>
+        <p className="text-sm lg:text-lg">
+          {formattedDate(weatherData?.time, "time", false)} -
+          {formattedDate(weatherData?.time, "date", false)}
+        </p>
       </div>
     </>
   );
