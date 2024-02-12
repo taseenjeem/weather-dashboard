5dadee96f2d54d10e5c9cbdb743dfb16648453df
taseenjeem <taseenjeem.dev@gmail.com>
2024-02-13 00:12:21 +0600

    added favorite provider

---
+++ src/provider/FavoriteProvider.jsx
@@ -0,0 +1,32 @@
+import { FavoriteContext } from "../context";
+import { useLocalStorage } from "../hooks";
+
+const FavoriteProvider = ({ children }) => {
+  const [favorites, setFavorites] = useLocalStorage("favorites", []);
+
+  const addToFavorites = (latitude, longitude, location) => {
+    setFavorites([
+      ...favorites,
+      {
+        latitude: latitude,
+        longitude: longitude,
+        location: location,
+      },
+    ]);
+  };
+
+  const removeFromFavorites = (location) => {
+    const restFavorites = favorites.filter((fav) => fav.location !== location);
+    setFavorites(restFavorites);
+  };
+
+  return (
+    <FavoriteContext.Provider
+      value={{ addToFavorites, removeFromFavorites, favorites }}
+    >
+      {children}
+    </FavoriteContext.Provider>
+  );
+};
+
+export default FavoriteProvider;
